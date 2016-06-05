ae2ce25b61a621e770b5b649d2f47cd3c6236429
Subhajit Sahu <wolfram77@gmail.com>
2016-06-05 22:17:40 +0530

    program: updated

--- cs-env/Program.cs
+++ cs-env/Program.cs
@@ -30,9 +30,10 @@ namespace orez.env {
 		/// <param name="args">Input parameters.</param>
 		static void Main(string[] args) {
 			var p = GetOpt(args);
-			var key = (p.args.Count == 0 ? "" : p.args[0]).ToLower();
+			var key = (p.args.Length == 0 ? "" : p.args[0]).ToLower();
 			key = Cmd.ContainsKey(key) ? key : "";
-      Cmd[key](p);
+			try { Cmd[key](p); }
+			catch(Exception e) { Console.Error.WriteLine("err: "+e.Message.ToLower()); }
 		}
 
 		/// <summary>
@@ -49,7 +50,7 @@ namespace orez.env {
 		/// </summary>
 		/// <param name="p">Input parameters.</param>
 		private static void Get(oParams p) {
-			var key = p.args.Count >= 2 ? p.args[1] : "";
+			var key = p.args.Length > 1 ? p.args[1] : "";
 			var val = Environment.GetEnvironmentVariable(key, p.mode);
 			if(val != null) Console.WriteLine(val);
 		}
@@ -58,8 +59,8 @@ namespace orez.env {
 		/// </summary>
 		/// <param name="p">Input parameters.</param>
 		private static void Set(oParams p) {
-			if(p.args.Count < 2) return;
-			string key = p.args[1], val = p.args.Count >= 3 ? p.args[2] : "";
+			if(p.args.Length < 2) return;
+			string key = p.args[1], val = p.args.Length > 2 ? p.args[2] : "";
 			Environment.SetEnvironmentVariable(key, val, p.mode);
 		}
 		/// <summary>
@@ -67,16 +68,14 @@ namespace orez.env {
 		/// </summary>
 		/// <param name="p">Input parameters.</param>
 		private static void Delete(oParams p) {
-			if(p.args.Count >= 2) Environment.SetEnvironmentVariable(p.args[1], null, p.mode);
-			else Console.Error.WriteLine("delete: invalid varible");
+			if(p.args.Length > 1) Environment.SetEnvironmentVariable(p.args[1], null, p.mode);
 		}
 		/// <summary>
 		/// Tells whether a sub-value is in environment variable.
 		/// </summary>
 		/// <param name="p">Input parameters.</param>
 		private static void Has(oParams p) {
-			if(p.args.Count < 2) return;
-			string key = p.args[1], sub = p.args.Count >= 3 ? p.args[2] : "";
+			string key = p.args.Length > 1 ? p.args[1] : "", sub = p.args.Length > 2 ? p.args[2] : "";
 			var val = Environment.GetEnvironmentVariable(key, p.mode);
 			Console.WriteLine(val != null ? (Array.IndexOf(val.Split(';'), sub) >= 0 ? 1 : 0) : 0);
 		}
@@ -85,10 +84,13 @@ namespace orez.env {
 		/// </summary>
 		/// <param name="p">Input parameters.</param>
 		private static void Add(oParams p) {
-			if(p.args.Count < 2) return;
-			string key = p.args[1], sub = p.args.Count >= 3 ? p.args[2] : "";
+			if(p.args.Length < 2) return;
+			string key = p.args[1], sub = p.args.Length > 2 ? p.args[2] : "";
 			var val = Environment.GetEnvironmentVariable(key, p.mode);
-			val = val != null && val.Length > 0 ? val + ";" + sub : sub;
+			var arr = val == null || val == "" ? new string[0] : val.Split(';');
+			var lst = new List<string>(arr);
+      if(!lst.Contains(sub)) lst.Add(sub);
+			val = string.Join(";", lst);
 			Environment.SetEnvironmentVariable(key, val, p.mode);
 		}
 		/// <summary>
@@ -96,10 +98,11 @@ namespace orez.env {
 		/// </summary>
 		/// <param name="p">Input parameters.</param>
 		private static void Remove(oParams p) {
-			if(p.args.Count < 2) return;
-			string key = p.args[1], sub = p.args.Count >= 3 ? p.args[2] : "";
+			if(p.args.Length < 2) return;
+			string key = p.args[1], sub = p.args.Length > 2 ? p.args[2] : "";
 			var val = Environment.GetEnvironmentVariable(key, p.mode);
-			var lst = new List<string>(val.Split(';'));
+			var arr = val == null || val == "" ? new string[0] : val.Split(';');
+			var lst = new List<string>(arr);
 			lst.Remove(sub);
 			val = string.Join(";", lst);
 			Environment.SetEnvironmentVariable(key, val, p.mode);
@@ -119,7 +122,9 @@ namespace orez.env {
 						p.mode = EnvironmentVariableTarget.Machine;
 						break;
 					default:
-						p.args.Add(args[i]);
+						p.args = new string[args.Length - i];
+						Array.Copy(args, i, p.args, 0, p.args.Length);
+						i = args.Length;
 						break;
 				}
 			}
