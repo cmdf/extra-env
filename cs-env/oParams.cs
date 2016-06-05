9e8423945062ba184fc65b8f3c7d89ae826ddb91
Subhajit Sahu <wolfram77@gmail.com>
2016-06-05 21:27:11 +0530

    oparams: cute comments added for mode

--- cs-env/oParams.cs
+++ cs-env/oParams.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 namespace orez.env {
 	class oParams {
 
+		/// <summary>
+		/// Indicates mode to use for getting or setting environment variable.
+		/// </summary>
 		public EnvironmentVariableTarget mode = EnvironmentVariableTarget.User;
 		public IList<string> args = new List<string>();
 	}
