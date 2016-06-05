2ae398855c3de0758ee0d79d53d700475027e49f
Subhajit Sahu <wolfram77@gmail.com>
2016-06-05 21:11:14 +0530

    project: created

---
+++ cs-env/oParams.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace cs_env {
+	class oParams {
+
+		public EnvironmentVariableTarget mode = EnvironmentVariableTarget.User;
+		public IList<string> args = new List<string>();
+	}
+}
